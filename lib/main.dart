8cae113f6fadabf3618dfce298afef93c507d325
Victor Bonnet <victor.bonnet@gmail.com>
2019-03-17 20:31:46 +0100

    Reduce code size

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,23 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    var tt = Theme.of(context).textTheme.apply(displayColor: Colors.white70, bodyColor: Colors.white70);
-
+  Widget build(BuildContext c) {
+    var t = Theme.of(c)
+        .textTheme
+        .apply(displayColor: Colors.white70, bodyColor: Colors.white70);
     return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        textTheme: tt,
-      ),
-      home: FWHome(),
+      theme: ThemeData(brightness: Brightness.dark, textTheme: t),
+      home: Home(),
     );
   }
 }
 
-class FWHome extends StatefulWidget {
+class Home extends StatefulWidget {
   @override
-  _FWState createState() => _FWState();
+  _HState createState() => _HState();
 }
 
-class _FWState extends State<FWHome> {
+class _HState extends State<Home> {
   List _cs;
   Map _c;
   double _w = 0;
@@ -33,7 +31,6 @@ class _FWState extends State<FWHome> {
   @override
   void initState() {
     super.initState();
-
     DefaultAssetBundle.of(context).loadString("assets/in.json").then((d) {
       _cs = json.decode(d);
       setState(() => _c = _cs[0]);
@@ -51,26 +48,22 @@ class _FWState extends State<FWHome> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.all(24.0),
-              child: Text(
-                "Fancy Wallet",
-                style: Theme.of(c).textTheme.display3.copyWith(
-                      fontFamily: 'rms',
-                      color: Colors.white,
-                    ),
-              ),
-            ),
+                padding: EdgeInsets.all(24.0),
+                child: Text(
+                  "Fancy Wallet",
+                  style: Theme.of(c)
+                      .textTheme
+                      .display3
+                      .copyWith(fontFamily: 'rms', color: Colors.white),
+                )),
             SizedBox(height: 32.0),
             CardSelector(
-              cards: _cs.map((c) {
-                return Card(c);
-              }).toList(),
-              mainCardWidth: _w,
-              mainCardHeight: _w * 0.63,
-              mainCardPadding: -16.0,
-              onChanged: (i) => setState(() => _c = _cs[i]),
-            ),
-            Expanded(child: AmountWidget(_c)),
+                cards: _cs.map((c) => Card(c)).toList(),
+                mainCardWidth: _w,
+                mainCardHeight: _w * 0.63,
+                mainCardPadding: -16.0,
+                onChanged: (i) => setState(() => _c = _cs[i])),
+            Expanded(child: Amounts(_c)),
           ],
         ),
       ),
@@ -78,59 +71,51 @@ class _FWState extends State<FWHome> {
   }
 }
 
-class AmountWidget extends StatelessWidget {
+class Amounts extends StatelessWidget {
   final Map _c;
 
-  AmountWidget(this._c);
+  Amounts(this._c);
 
   @override
-  Widget build(BuildContext context) {
-    var tt = Theme.of(context).textTheme;
+  Widget build(BuildContext cx) {
+    var tt = Theme.of(cx).textTheme;
     var pd = EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0);
     return ListView.builder(
       physics: BouncingScrollPhysics(),
-      itemCount: (_c['trxs'] as List).length + 1,
+      itemCount: (_c['tx'] as List).length + 1,
       itemBuilder: (c, i) {
         if (i == 0) {
           return Padding(
-            padding: pd,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Balance', style: tt.caption),
-                SizedBox(height: 8.0),
-                Text(_c['bl'], style: tt.display1.apply(color: Colors.white)),
-                SizedBox(height: 24.0),
-                Text('Today', style: tt.caption),
-              ],
-            ),
-          );
+              padding: pd,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Balance', style: tt.caption),
+                    SizedBox(height: 8.0),
+                    Text(_c['bl'],
+                        style: tt.display1.apply(color: Colors.white)),
+                    SizedBox(height: 24.0),
+                    Text('Today', style: tt.caption),
+                  ]));
         }
-
-        var trx = _c['trxs'][i - 1];
+        var tx = _c['tx'][i - 1];
         return Padding(
-          padding: pd,
-          child: Row(
-            children: <Widget>[
-              Icon(
-                MdiIcons.fromString(trx['i']),
-                size: 24.0,
-                color: Colors.blueGrey,
-              ),
+            padding: pd,
+            child: Row(children: <Widget>[
+              Icon(MdiIcons.fromString(tx['i']),
+                  size: 24.0, color: Colors.blueGrey),
               SizedBox(width: 16.0),
               Expanded(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(trx['m'], style: tt.title.apply(color: Colors.white)),
-                    Text(trx['t'], style: tt.caption),
+                    Text(tx['m'], style: tt.title.apply(color: Colors.white)),
+                    Text(tx['t'], style: tt.caption)
                   ],
                 ),
               ),
-              Text(trx['a'], style: tt.body2.apply(color: Colors.deepOrange))
-            ],
-          ),
-        );
+              Text(tx['a'], style: tt.body2.apply(color: Colors.deepOrange))
+            ]));
       },
     );
   }
@@ -145,37 +130,33 @@ class Card extends StatelessWidget {
   Widget build(BuildContext context) {
     var tt = Theme.of(context).textTheme;
     return ClipRRect(
-      borderRadius: BorderRadius.circular(12.0),
-      child: Container(
-        color: Color(_c['co']),
-        child: Stack(
-          children: <Widget>[
-            Image.asset('assets/${_c['txt']}.png',
-                fit: BoxFit.cover,
-                height: double.infinity,
-                width: double.infinity,
-                alignment: Alignment.center),
-            Padding(
-              padding: EdgeInsets.all(24.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(_c['bk'], style: tt.title),
-                  Text(_c['ty'].toUpperCase(), style: tt.caption),
-                  Expanded(child: Container()),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Expanded(child: Text(_c['num'], style: tt.subhead)),
-                      Image.asset('assets/${_c['br']}.png', width: 48.0)
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+        borderRadius: BorderRadius.circular(12.0),
+        child: Container(
+            color: Color(_c['co']),
+            child: Stack(children: <Widget>[
+              Image.asset('assets/${_c['txt']}.png',
+                  fit: BoxFit.cover,
+                  height: double.infinity,
+                  width: double.infinity),
+              Padding(
+                  padding: EdgeInsets.all(24.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(_c['bk'], style: tt.title),
+                        Text(_c['ty'].toUpperCase(), style: tt.caption),
+                        Expanded(child: Container()),
+                        Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: <Widget>[
+                              Expanded(
+                                child: Text(_c['nm'],
+                                    style: tt.subhead,
+                                    overflow: TextOverflow.ellipsis),
+                              ),
+                              Image.asset('assets/${_c['br']}.png', width: 48.0)
+                            ])
+                      ])),
+            ])));
   }
 }
